2a63bd251a2c00a49597ac3cfea21556339eb3ee
budicmarin <55134480+budicmarin@users.noreply.github.com>
2020-05-19 12:38:53 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace budicMarinTextWriter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonUpis = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonUpis
+            // 
+            this.buttonUpis.Location = new System.Drawing.Point(13, 355);
+            this.buttonUpis.Name = "buttonUpis";
+            this.buttonUpis.Size = new System.Drawing.Size(75, 23);
+            this.buttonUpis.TabIndex = 0;
+            this.buttonUpis.Text = "Upisi";
+            this.buttonUpis.UseVisualStyleBackColor = true;
+            this.buttonUpis.Click += new System.EventHandler(this.buttonUpis_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(747, 318);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonUpis);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonUpis;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace budicMarinTextWriter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonUpis_Click(object sender, EventArgs e)
+        {
+            string fullPath = @"D:\Temp\temp.txt";
+            string lines = textBox1.Text;
+            using (StreamWriter writer = new StreamWriter(fullPath))
+            {
+                writer.Write(lines + "\n");
+                writer.WriteLine(" ");
+            }
+        }
+    }
+}
